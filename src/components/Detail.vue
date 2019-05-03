b2fb746f13b5698edaa21fa0e2f07554c78884ee
Jordan Gunther <jordankgunther@gmail.com>
2019-05-03 16:21:03 -0600

    added movie details to details page and a back to results button, also removed the temporary v-bind to session times

--- src/components/Detail.vue
+++ src/components/Detail.vue
@@ -1,6 +1,20 @@
 <template>
     <div id="detail" v-if="movie">
-        <movie-item v-bind:movie="movie.movie"></movie-item>
+        <movie-item v-bind:movie="movie">
+            <div class="movie-details">
+                <p class="movie-genre">{{ movie.Genre }}</p>
+                <p class="movie-plot">{{ movie.Plot }}</p>
+                <table>
+                    <tr><td>Released date:</td><td>{{ movie.Released }}</td></tr>
+                    <tr><td>Running time:</td><td>{{ movie.Runtime }}</td></tr>
+                    <tr><td>Director:</td><td>{{ movie.Director }}</td></tr>
+                    <tr><td>Cast:</td><td>{{ movie.Actors }}</td></tr>
+                </table>
+            </div>
+        </movie-item>
+        <div class="home">
+            <router-link v-bind:to="{ name: 'home' }">Back to results</router-link>
+        </div>
     </div>
 </template>
 
@@ -13,7 +27,7 @@
               // filter through movies and return true for all movies with the id from the route param id
               let movie = this.movies.find(movie => movie.id === this.$route.params.id);
               // return null if no movie was found
-              return movie ? movie : null;
+              return movie ? movie.movie : null;
           }
         },
         components:  {

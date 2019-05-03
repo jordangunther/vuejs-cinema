b2fbfecc599b9cedcd86be9842f74a215faa934a
Jordan Gunther <jordankgunther@gmail.com>
2019-05-03 16:05:57 -0600

    added movie item component to details page including filter to grab the movie that corresponds to the id. temporarily bound sessions to a blank array to avoid error, will handle later

--- src/components/Detail.vue
+++ src/components/Detail.vue
@@ -1,11 +1,23 @@
 <template>
-    <div>Hello world</div>
+    <div id="detail" v-if="movie">
+        <movie-item v-bind:movie="movie.movie" v-bind:sessions="[]"></movie-item>
+    </div>
 </template>
 
 <script>
+    import MovieItem from './MovieItem.vue';
     export default {
-        created() {
-            console.log(this.$route.params.id);
+        props: [ 'movies'],
+        computed: {
+          movie() {
+              // filter through movies and return true for all movies with the id from the route param id
+              let movie = this.movies.find(movie => movie.id === this.$route.params.id);
+              // return null if no movie was found
+              return movie ? movie : null;
+          }
+        },
+        components:  {
+            MovieItem
         }
     }
 </script>
